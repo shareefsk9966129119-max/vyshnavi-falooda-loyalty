9a935267c6d3ebe8f67893c600d1edebea91d170
Shaik Shareef <shareefsk9966129119@gmail.com>
2026-04-01 01:09:21 +0530

    Updated referral system and passwords

--- models/Referral.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const referralSchema = new mongoose.Schema({
-    referredBy: String,
-    referredPhone: { type: String, unique: true } // 🔥 prevents duplicates
-});
-
-module.exports = mongoose.model("Referral", referralSchema);
